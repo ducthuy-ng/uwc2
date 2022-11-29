e188e3c4e88ad2a1e48feb0b2b5d5557dc05a8ad
nqduy65 <nguyenquocduy06051998@gmail.com>
2022-11-29 11:51:28 +0700

    add ja_id in query, delete check results.rows.length,split export default + async function, change function name

--- pages/api/jatrolley/index.ts
+++ pages/api/jatrolley/index.ts
@@ -4,23 +4,18 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { resourceLimits } from "worker_threads";
 import { query } from "../../../lib/postgres";
 
-export default async function get_trolley_ja(req:NextApiRequest,res:NextApiResponse) {
+async function get_ja_and_assignment(req:NextApiRequest,res:NextApiResponse) {
     let ja_id =req.query["ja_id"];
     let trolley_id = req.query["trolley_id"];
     try{
-        let results = await query('select full_name, trolley_id from employee left join\
+        let results = await query('select id as "ja_id", full_name as "ja_name", trolley_id from employee left join\
         trolley_assignment\
-        on employee.id = trolley_assignment.ja_id') 
-        if(results.rows.length <1){
-            res.status(403).send({message:'dm'});
-            return;
-        }
-        res.status(200).send(results.rows)
-
-        
+        on employee.id = trolley_assignment.ja_id')
+        res.status(200).send(results.rows)   
     }
     catch(e){
         res.status(500).send({})
         return
     }
 }
+export default get_ja_and_assignment;
