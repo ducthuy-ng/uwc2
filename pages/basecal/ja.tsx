d71117fbefae577e5ec971b07cec4f052e6129fe
Nguyen-Kim-Tien <tien.nguyenctcxht11@hcmut.edu.vn>
2022-11-30 02:02:24 +0700

    add basecal ja

---
+++ pages/basecal/ja.tsx
@@ -0,0 +1,53 @@
+import type { NextPage } from "next";
+import styles from "./../../styles/ja.module.css";
+import { Button } from "./../../components/Button/Button";
+import { Input } from "./../../components/Input/Input";
+
+const ja: NextPage = (props: AssignmentProps) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>QUẢN LÝ LỊCH NỀN LAO CÔNG - lao công</h1>
+      </div>
+      <div className={styles.option}>
+        <div>
+          <Button>Quay lại</Button>
+        </div>
+        <div className={styles.optionSearch}>
+          <div>
+            <Input className={styles.optionSearch_Input} placeholder="Nhập tên lao công ..."></Input>
+          </div>
+          <div>
+            <Button className={styles.optionSearch_Button}>Tìm kiếm</Button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.body}>
+        <div style={{ fontWeight: "bold" }}>
+          <Assignment name="Họ và tên" id="MSNV"></Assignment>
+        </div>
+        <Assignment name="Nguyễn Văn A" id="5/7"></Assignment>
+        <Assignment name="Nguyễn Văn B" id="0/7"></Assignment>
+        <Assignment name="Nguyễn Văn C" id="1/7"></Assignment>
+        <Assignment name="Nguyễn Văn D" id="7/7"></Assignment>
+        <Assignment name="Nguyễn Văn E" id="3/7"></Assignment>
+      </div>
+    </div>
+  );
+};
+
+type AssignmentProps = {
+  name?: string;
+  id?: string;
+};
+
+const Assignment = (props: AssignmentProps) => {
+  return (
+    <div className={styles.item}>
+      <div className={styles.item_Name}>{props.name}</div>
+      <div className={styles.item_MSNV}>{props.id}</div>
+    </div>
+  );
+};
+
+export default ja;
