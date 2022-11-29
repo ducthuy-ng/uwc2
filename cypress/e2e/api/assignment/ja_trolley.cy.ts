187925a45743079b4968cfb8e61378e8cddfed31
ducthuy-ng <46134513+ducthuy-ng@users.noreply.github.com>
2022-11-29 22:03:32 +0700

    Update unittests for clearer specification

--- cypress/e2e/api/assignment/ja_trolley.cy.ts
+++ cypress/e2e/api/assignment/ja_trolley.cy.ts
@@ -6,7 +6,7 @@ describe("Test Assign JA-Trolley", () => {
     }).then(resp => {
       expect(resp.body).to.have.length(10);
       for (const assigment of resp.body) {
-        expect(assigment).to.have.all.keys(["ja_id", "ja_name", "trolley_id"])
+        expect(assigment).to.have.all.keys(["ja_id", "ja_name", "trolley_id"]);
         expect(assigment["trolley_id"]).to.be.null;
       }
     });
@@ -31,7 +31,7 @@ describe("Test Assign JA-Trolley", () => {
       url: "/api/assignment/ja-trolley",
       method: "GET"
     }).then(resp => {
-      expect(resp.body).to.deep.include({ "ja_id": 1, "trolley_id": 1 });
+      expect(resp.body).to.deep.include({ "ja_id": 1, "ja_name": "Susan", "trolley_id": 1 });
     });
   });
 
@@ -42,7 +42,8 @@ describe("Test Assign JA-Trolley", () => {
       qs: {
         ja_id: 1,
         trolley_id: 2
-      }
+      },
+      failOnStatusCode: false
     }).then(resp => {
       expect(resp.status).to.be.eq(400);
     });
@@ -55,7 +56,8 @@ describe("Test Assign JA-Trolley", () => {
       qs: {
         ja_id: 2,
         trolley_id: 1
-      }
+      },
+      failOnStatusCode: false
     }).then(resp => {
       expect(resp.status).to.be.eq(400);
     });
@@ -81,7 +83,21 @@ describe("Test Assign JA-Trolley", () => {
       qs: {
         trolley_id: 1,
         ja_id: 1
-      }
+      },
+      failOnStatusCode: false
+    }).then(resp => {
+      expect(resp.status).to.be.eq(400);
+    });
+  });
+
+  it("Delete but missing query should fail", () => {
+    cy.request({
+      url: "/api/assignment/ja-trolley",
+      method: "DELETE",
+      qs: {
+        trolley_id: 1,
+      },
+      failOnStatusCode: false
     }).then(resp => {
       expect(resp.status).to.be.eq(400);
     });
