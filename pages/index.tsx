ad88d9f959d978ba2448cbb711905078fb49ca13
ducthuy-ng <46134513+ducthuy-ng@users.noreply.github.com>
2022-11-29 15:50:00 +0700

    Create backbone for Homepage modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
 import type { NextPage } from "next";
 
-import styles from "./../styles/Homepage.module.css"
+import styles from "./../styles/Homepage.module.css";
+import { Dispatch, SetStateAction, useState } from "react";
 
 const Home: NextPage = () => {
+  const [showJATrolleyModal, setShowJATrolleyModal] = useState(false);
+
   return (
     <div className={styles.Trang_chu}>
-      <div> 
+      <div>
         <h1 className={styles.Homepage_title}>TRANG CHỦ</h1>
       </div>
       <div className={styles.Actions_Board}>
         <div className={styles.Column}>
           <button className={styles.button}> Phân xe đẩy - lao công</button>
-          <button className={styles.button}> Quản lý lịch nền - lao công</button>
+          <button
+            className={styles.button}
+            onClick={() => {
+              setShowJATrolleyModal(true);
+            }}
+          >
+            Quản lý lịch nền - lao công
+          </button>
           <button className={styles.button}> Lịch làm việc - lao công</button>
         </div>
 
@@ -21,8 +31,27 @@ const Home: NextPage = () => {
           <button className={styles.button}> Lịch làm việc - tài xế</button>
         </div>
       </div>
+
+      {showJATrolleyModal ? <AssignJATrolleyModal setShowModal={setShowJATrolleyModal} /> : null}
     </div>
   );
 };
+
+type ModalProps = {
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+};
+
+function AssignJATrolleyModal(props: ModalProps) {
+  return (
+    <div
+      className={styles.modal_background}
+      onClick={() => {
+        props.setShowModal(false);
+      }}
+    >
+      <div className={styles.modal}>abc</div>
+    </div>
+  );
+}
 
 export default Home;
