9560ab78e34f53a17ca8d6f5f42cc0bfa7bd1626
sKomuKomu <75112005+Khang-CS@users.noreply.github.com>
2022-11-30 17:45:36 +0700

    Connect frontend with backend

--- pages/assignment-vehicle.tsx
+++ pages/assignment-vehicle.tsx
@@ -2,9 +2,12 @@ import type { NextPage } from "next";
 import styles from "./../styles/assignment-vehicle.module.css";
 import { Button } from "../components/Button/Button";
 import { Input } from "../components/Input/Input";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { fetcher } from "../lib/fetch";
+import useSWR from "swr";
 
 const Home: NextPage = () => {
+  const { data, error } = useSWR("/api/assignment/area-vehicle", fetcher);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -27,31 +30,65 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div className={styles.body}>
-        <Assignment area="Phường P1, Quận Q1"></Assignment>
-        <Assignment area="Phường P2, Quận Q1"></Assignment>
-        <Assignment area="Phường P3, Quận Q1"></Assignment>
-        <Assignment area="Phường P4, Quận Q1"></Assignment>
-        <Assignment area="Phường P1, Quận Q2"></Assignment>
-        <Assignment area="Phường P2, Quận Q2"></Assignment>
-        <Assignment area="Phường P1, Quận Q3"></Assignment>
+         {data
+          ? data.map((item: AssignmentProps) => (
+              <Assignment
+                area_name={item["area_name"]}
+                area_id={item["area_id"]}
+                vehicle_id={item["vehicle_id"]}
+              />
+            ))
+          : "Loading..."}
       </div>
     </div>
   );
 };
 
 type AssignmentProps = {
-  area?: string;
+  area_name: string;
+  area_id: string;
+  vehicle_id?: string;
+
 };
 
 const Assignment = (props: AssignmentProps) => {
+  const [value, setValue] = useState(props.vehicle_id || "");
   return (
-    <div className={styles.item}>
-      <div className={styles.item_Text}>{props.area}</div>
+    <form
+      onSubmit={async (event) => {
+        event.preventDefault();
+        await formSubmit(props.area_id, value, props.vehicle_id);
+      }}
+      className={styles.item}
+    >
+      <div className={styles.item_Text}>{props.area_name}</div>
       <div className={styles.item_Input}>
-        <Input></Input>
+        <Input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        ></Input>
       </div>
-    </div>
+      <input type="submit" hidden></input>
+    </form>
   );
 };
+
+async function formSubmit(area_id: string, value: string, vehicle_id?: string) {
+  if (value) {
+    await fetch(`/api/assignment/area-vehicle?area_id=${area_id}&vehicle_id=${value}`, {
+      method: "POST",
+    });
+
+    return;
+  }
+
+  if (vehicle_id) {
+    await fetch(`/api/assignment/area-vehicle?area_id=${area_id}&vehicle_id=${vehicle_id}`, {
+      method: "DELETE",
+    });
+  }
+}
 
 export default Home;
