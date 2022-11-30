9c00698459fe941879b918946ddf016780a2b24f
ducthuy-ng <46134513+ducthuy-ng@users.noreply.github.com>
2022-11-30 17:02:38 +0700

    Final QC & Run formatter

--- pages/assignment-ja.tsx
+++ pages/assignment-ja.tsx
@@ -2,13 +2,12 @@ import type { NextPage } from "next";
 import styles from "./../styles/assignment-ja.module.css";
 import { Button } from "../components/Button/Button";
 import { Input } from "../components/Input/Input";
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 import { fetcher } from "../lib/fetch";
 import useSWR from "swr";
-import { stringify } from "querystring";
 
 const Home: NextPage = () => {
-  const { data, error } = useSWR("/api/assignment/ja-trolley", fetcher)
+  const { data, error } = useSWR("/api/assignment/ja-trolley", fetcher);
 
   return (
     <div className={styles.container}>
@@ -32,47 +31,64 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div className={styles.body}>
-        {data ? data.map(item => <Assignment key={item['ja_id']} ja={item['ja_name']} ja_id={item['ja_id']} trolley_id={item['trolley_id']} />) : null}
+        {data
+          ? data.map((item: AssignmentProps) => (
+              <Assignment
+                key={item["ja_id"]}
+                ja_name={item["ja_name"]}
+                ja_id={item["ja_id"]}
+                trolley_id={item["trolley_id"]}
+              />
+            ))
+          : "Loading..."}
       </div>
     </div>
   );
 };
 
 type AssignmentProps = {
-  ja: string;
+  ja_name: string;
   ja_id: string;
   trolley_id?: string;
 };
 
 const Assignment = (props: AssignmentProps) => {
-  const [value, setValue] = useState(props.trolley_id || "")
+  const [value, setValue] = useState(props.trolley_id || "");
   return (
-
-    <form onSubmit={() => {formSubmit(props.ja_id, value, props.trolley_id) }} className={styles.item}>
-      <div className={styles.item_Text}>{props.ja}</div>
+    <form
+      onSubmit={async (event) => {
+        event.preventDefault();
+        await formSubmit(props.ja_id, value, props.trolley_id);
+      }}
+      className={styles.item}
+    >
+      <div className={styles.item_Text}>{props.ja_name}</div>
       <div className={styles.item_Input}>
-        <Input value={value} onChange={(e) => {setValue(e.target.value) }}></Input>
+        <Input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        ></Input>
       </div>
       <input type="submit" hidden></input>
     </form>
   );
 };
 
-function formSubmit(ja_id: string, value: string, trolley_id?: string) {
-  console.log(value);
-  
+async function formSubmit(ja_id: string, value: string, trolley_id?: string) {
   if (value) {
-    fetch(`/api/assignment/ja-trolley?ja_id=${ja_id}&trolley_id=${value}`, {
-      method: "POST"
+    await fetch(`/api/assignment/ja-trolley?ja_id=${ja_id}&trolley_id=${value}`, {
+      method: "POST",
     });
 
     return;
   }
 
   if (trolley_id) {
-    fetch(`/api/assignment/ja-trolley?ja_id=${ja_id}&trolley_id=${trolley_id}`, {
-      method: "DELETE"
-    })
+    await fetch(`/api/assignment/ja-trolley?ja_id=${ja_id}&trolley_id=${trolley_id}`, {
+      method: "DELETE",
+    });
   }
 }
 
