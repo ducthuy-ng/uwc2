cd323b65d0ef76fd4ef844b47c8e58169c325961
sKomuKomu <75112005+Khang-CS@users.noreply.github.com>
2022-11-30 16:11:55 +0700

    connect with backend

--- pages/assignment-ja.tsx
+++ pages/assignment-ja.tsx
@@ -2,9 +2,14 @@ import type { NextPage } from "next";
 import styles from "./../styles/assignment-ja.module.css";
 import { Button } from "../components/Button/Button";
 import { Input } from "../components/Input/Input";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { fetcher } from "../lib/fetch";
+import useSWR from "swr";
+import { stringify } from "querystring";
 
 const Home: NextPage = () => {
+  const { data, error } = useSWR("/api/assignment/ja-trolley", fetcher)
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -27,31 +32,48 @@ const Home: NextPage = () => {
         </div>
       </div>
       <div className={styles.body}>
-        <Assignment ja="Nguyễn Văn A"></Assignment>
-        <Assignment ja="Nguyễn Văn B"></Assignment>
-        <Assignment ja="Nguyễn Văn C"></Assignment>
-        <Assignment ja="Nguyễn Văn D"></Assignment>
-        <Assignment ja="Nguyễn Văn E"></Assignment>
-        <Assignment ja="Nguyễn Văn F"></Assignment>
-        <Assignment ja="Nguyễn Văn G"></Assignment>
+        {data ? data.map(item => <Assignment key={item['ja_id']} ja={item['ja_name']} ja_id={item['ja_id']} trolley_id={item['trolley_id']} />) : null}
       </div>
     </div>
   );
 };
 
 type AssignmentProps = {
-  ja?: string;
+  ja: string;
+  ja_id: string;
+  trolley_id?: string;
 };
 
 const Assignment = (props: AssignmentProps) => {
+  const [value, setValue] = useState(props.trolley_id || "")
   return (
-    <div className={styles.item}>
+
+    <form onSubmit={() => {formSubmit(props.ja_id, value, props.trolley_id) }} className={styles.item}>
       <div className={styles.item_Text}>{props.ja}</div>
       <div className={styles.item_Input}>
-        <Input></Input>
+        <Input value={value} onChange={(e) => {setValue(e.target.value) }}></Input>
       </div>
-    </div>
+      <input type="submit" hidden></input>
+    </form>
   );
 };
+
+function formSubmit(ja_id: string, value: string, trolley_id?: string) {
+  console.log(value);
+  
+  if (value) {
+    fetch(`/api/assignment/ja-trolley?ja_id=${ja_id}&trolley_id=${value}`, {
+      method: "POST"
+    });
+
+    return;
+  }
+
+  if (trolley_id) {
+    fetch(`/api/assignment/ja-trolley?ja_id=${ja_id}&trolley_id=${trolley_id}`, {
+      method: "DELETE"
+    })
+  }
+}
 
 export default Home;
