e28f525828b9dbff782211cedbc5e2357878c6b0
BuiThanhBinh <binh.s078251@gmail.com>
2022-11-30 16:09:52 +0700

    BaseCalendar update

---
+++ pages/api/ja_base_calendar/co-base.ts
@@ -0,0 +1,106 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { query } from "../../../lib/postgres";
+
+export enum day_of_week{
+  'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
+}
+
+export default async function AssignmentCoTrolley(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case "GET":
+      await getCoBaseCalendar(req, res);
+      break;
+    case "POST":
+      await createCoBaseCalendar(req, res);
+      break;
+    case "DELETE":
+      await deleteCoBaseCalendar(req, res);
+      break;
+    default:
+      res.status(404).send({ message: "Method not found" });
+      break;
+  }
+}
+
+async function getCoBaseCalendar(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    let results = await query("select id, coalesce(jbc.count, 0) as number_day_of_week from area left join\
+    (select area_id, count(*) from co_base_calendar group by area_id order by area_id asc) jbc on area.id = jbc.area_id");
+    res.status(200).send(results.rows);
+  } catch (e) {
+    res.status(500).send({});
+    return;
+  }
+}
+
+async function createCoBaseCalendar(req: NextApiRequest, res: NextApiResponse) {
+  let area = req.query["area_id"];
+  let co = req.query["co_id"];
+  let day = req.query["day_of_week"];
+  //check not null
+  if (!area || !co || !day) {
+    res.status(400).send({ message: "Input should not null" });
+    return;
+  }
+  try {
+    //select query
+    let result_co = await query("SELECT * FROM employee WHERE id = $1 and working_role = 'CO'", [co]);
+    let result_area = await query("SELECT * FROM area WHERE id = $1", [area]);
+    //check if co and area are existed
+    if (result_co.rows.length != 1 || result_area.rows.length != 1) {
+      res.status(400).send({ message: "area or co is not correct" });
+      return;
+    }
+    //check if day is valid
+    if (!Object.values(day_of_week).includes(String(day))) {
+      res.status(400).send({ message: "Day value is not valid" });
+      return;
+    }
+    //check if area has already assigned in that day
+    result_area = await query("SELECT * FROM co_base_calendar\
+    WHERE area_id = $1 and day_of_week = $2", [area, day]);
+    if (result_area.rows.length != 0) {
+      res.status(400).send({ message: "area has already assigned in this day" });
+      return;
+    }
+    //insert values into co_base_calendar
+    await query("INSERT INTO co_base_calendar(area_id, co_id, day_of_week) values ($1, $2, $3);", [area, co, day]);
+  } catch (e) {
+    res.status(400).send({ message: "Error" });
+    return;
+  }
+  //200 success
+  res.status(200).send({});
+}
+
+async function deleteCoBaseCalendar(req: NextApiRequest, res: NextApiResponse) {
+  let area = req.query["area_id"];
+  let co = req.query["co_id"];
+  let day = req.query["day_of_week"];
+  //check not null
+  if (!area || !co || !day) {
+    res.status(400).send({ message: "Input should not null" });
+    return;
+  }
+  try {
+    let results = await query("SELECT * FROM co_base_calendar \
+    WHERE co_id = $1 and area_id = $2 and day_of_week = $3", [co, area, day]);
+    //check if co base calendar is existed
+    if (results.rows.length < 1) {
+      res.status(400).send({ message: "There are no such calendar" });
+      return;
+    }
+    //check if day is valid
+    if (!Object.values(day_of_week).includes(String(day))) {
+      res.status(400).send({ message: "Day value is not valid" });
+      return;
+    }
+    //delete row
+    await query("DELETE FROM co_base_calendar WHERE co_id = $1 and area_id = $2 and day_of_week = $3", [co, area, day]);
+  } catch(e) {
+    res.status(400).send({ message: "Error" });
+    return;
+  }
+  //200 success
+  res.status(200).send({});
+}
---
+++ pages/api/ja_base_calendar/ja-base.ts
@@ -0,0 +1,106 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { query } from "../../../lib/postgres";
+
+export enum day_of_week{
+  'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'
+}
+
+export default async function AssignmentJABaseCalendar(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case "GET":
+      await getJABaseCalendar(req, res);
+      break;
+    case "POST":
+      await createJaBaseCalendar(req, res);
+      break;
+    case "DELETE":
+      await deleteJaBaseCalendar(req, res);
+      break;
+    default:
+      res.status(404).send({ message: "Method not found" });
+      break;
+  }
+}
+
+async function getJABaseCalendar(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    let results = await query("select id, coalesce(jbc.count, 0) as number_day_of_week from mcp left join\
+    (select mcp_id, count(*) from ja_base_calendar group by mcp_id order by mcp_id asc) jbc on mcp.id = jbc.mcp_id");
+    res.status(200).send(results.rows);
+  } catch (e) {
+    res.status(500).send({});
+    return;
+  }
+}
+
+async function createJaBaseCalendar(req: NextApiRequest, res: NextApiResponse) {
+  let mcp = req.query["mcp_id"];
+  let ja = req.query["ja_id"];
+  let day = req.query["day_of_week"];
+  //check not null
+  if (!mcp || !ja || !day) {
+    res.status(400).send({ message: "Input should not null" });
+    return;
+  }
+  try {
+    //select query
+    let result_ja = await query("SELECT * FROM employee WHERE id = $1 and working_role = 'JA'", [ja]);
+    let result_mcp = await query("SELECT * FROM mcp WHERE id = $1", [mcp]);
+    //check if ja and mcp are existed
+    if (result_ja.rows.length != 1 || result_mcp.rows.length != 1) {
+      res.status(400).send({ message: "Mcp or ja are not correct" });
+      return;
+    }
+    //check if day is valid
+    if (!Object.values(day_of_week).includes(String(day))) {
+      res.status(400).send({ message: "Day value is not valid" });
+      return;
+    }
+    //check if mcp has already assigned in that day
+    result_mcp = await query("SELECT * FROM ja_base_calendar\
+    WHERE mcp_id = $1 and day_of_week = $2", [mcp, day]);
+    if (result_mcp.rows.length != 0) {
+      res.status(400).send({ message: "Mcp has already assigned in this day" });
+      return;
+    }
+    //insert values into ja_base_calendar
+    await query("INSERT INTO ja_base_calendar(mcp_id, ja_id, day_of_week) values ($1, $2, $3);", [mcp, ja, day]);
+  } catch (e) {
+    res.status(400).send({ message: "Error" });
+    return;
+  }
+  //200 success
+  res.status(200).send({});
+}
+
+async function deleteJaBaseCalendar(req: NextApiRequest, res: NextApiResponse) {
+  let mcp = req.query["mcp_id"];
+  let ja = req.query["ja_id"];
+  let day = req.query["day_of_week"];
+  //check not null
+  if (!mcp || !ja || !day) {
+    res.status(400).send({ message: "Input should not null" });
+    return;
+  }
+  try {
+    let results = await query("SELECT * FROM ja_base_calendar \
+    WHERE ja_id = $1 and mcp_id = $2 and day_of_week = $3", [ja, mcp, day]);
+    //check if ja base calendar is existed
+    if (results.rows.length < 1) {
+      res.status(400).send({ message: "There are no such calendar" });
+      return;
+    }
+    //check if day is valid
+    if (!Object.values(day_of_week).includes(String(day))) {
+      res.status(400).send({ message: "Day value is not valid" });
+      return;
+    }
+    //delete row
+    await query("DELETE FROM ja_base_calendar WHERE ja_id = $1 and mcp_id = $2 and day_of_week = $3", [ja, mcp, day]);
+  } catch(e) {
+    res.status(400).send({ message: "Error" });
+    return;
+  }
+  //200 success
+  res.status(200).send({});
+}
