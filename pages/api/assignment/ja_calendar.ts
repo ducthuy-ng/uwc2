be72c8aa1c2f45a79ecbe3324698a07b9b3c84ba
trivien11vn <triviencoderr@gmail.com>
2022-12-01 04:10:20 +0700

    api_get_ja_calendar

---
+++ pages/api/assignment/ja_calendar.ts
@@ -0,0 +1,58 @@
+import { result } from "cypress/types/lodash";
+import { NextApiRequest, NextApiResponse } from "next";
+import { query } from "../../../lib/postgres";
+async function create_ja_calendar(req: NextApiRequest, res: NextApiResponse) {
+    let month = req.query["month"];
+    let year = req.query["year"];
+    try {
+        let number_days= new Date(Number(year), Number(month), 0).getDate();
+        for(var i=1;i<=number_days;i++){
+            let x= new Date(Number(year), Number(month)-1,i).getDay();
+            let y;
+            switch (x) {
+                case 0:
+                  y='Sun';
+                  break;
+                case 1:
+                  y='Mon';
+                  break;
+                case 2:
+                  y='Tue'
+                  break;
+                case 3:
+                  y='Wed'
+                  break;
+                case 4:
+                  y='Thu'
+                  break;
+                case 5:
+                  y='Fri'
+                  break;
+                case 6:
+                  y='Sat'
+                  break;
+            }
+            let results=await query("SELECT ja_id, mcp_id FROM ja_base_calendar WHERE day_of_week = $1", [y]);
+            for(var j=1;j<=results.rows.length;j++){
+                let date= year+"/"+month+"/"+i;
+                console.log(date);
+                await query("INSERT INTO ja_calendar (mcp_id,work_date,ja_id) VALUES ($1,TO_DATE($2,'YYYY/MM/DD'),$3)", [results.rows[j-1].mcp_id,date,results.rows[j-1].ja_id]);
+            }
+        }
+    } catch (e) {
+    res.status(500).send("hello");
+    return;
+    }
+    res.status(200).send({});
+}
+
+export default async function set_ja_calendar(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case "GET":
+      await create_ja_calendar(req, res);
+      break;
+    default:
+      res.status(404).send({ message: "Method not found" });
+      break;
+  }
+}
